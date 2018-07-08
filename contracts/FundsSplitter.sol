af7e603c9ebd15fdfa0de8a238e3ba0a41346c0d
furqanbaqai <baqai.furqan@gmail.com>
2018-07-08 20:00:57 +0400

    Revision#6: Simplifying mapping for better clarity

--- contracts/FundsSplitter.sol
+++ contracts/FundsSplitter.sol
@@ -1,31 +1,26 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.24;
 
 /**
  * @title Contract for keeping ledger of credit.
  * Credit will be divided into two and shall be assigned
  * to beneficiary 1 and beneficiary 2
  * Revision# 04: Incorporating Review comments by Rob.
- * Stretch Goals: After you addressed the small hints, refactor the contract so it can 
- * handle many splits from many people at the same time. Additionally, make it so each 
- * sender can name any two receivers. This means bob and carol are not special. You have 
- * to think about everyone.        
+ * Changes:
+ *      1. Making contract more simpler by changing ownership mechanisim.
  **/
 contract FundsSplitter {    
 
-    struct Transaction{
-        address sender;
-        address receiver;        
-        uint balance;               
-    }
+
     bool public paused;
     address public owner;
-    mapping(bytes32 => Transaction) public splitTransactions;
+
+    mapping(address => uint) public balances;
         
     
     event LogSplit(address indexed from, address receiver1, address receiver2, uint amount); /*Commit#4: Refactoring*/ 
     event LogWithdrawl(address indexed who, uint amount, uint balance); /*Commit#3: Index withdrawal */    
-    event LogPause(address sender);
-    event LogUnPause(address sender);
+    event LogPause(address initiator);
+    event LogUnPause(address initiator);
     
     /**
      * Modifier for checking if the procedure is called
@@ -69,10 +64,8 @@ contract FundsSplitter {
         require(receiver1 != receiver2, "[ER02] Invalid address");
         require(msg.value > 0, "[ER04] Invalid Values");
         uint amount = msg.value / 2;                
-        bytes32 key1 = keccak256(msg.sender,receiver1);
-        bytes32 key2 = keccak256(msg.sender,receiver2);
-        splitTransactions[key1] = Transaction(msg.sender,receiver1, splitTransactions[key1].balance+amount);
-        splitTransactions[key2] = Transaction(msg.sender,receiver2,splitTransactions[key2].balance +(msg.value - amount));        
+        balances[receiver1] += amount;
+        balances[receiver2] += msg.value - amount;
         emit LogSplit(msg.sender, receiver1,receiver2, msg.value);
         return true;
     }
@@ -81,17 +74,13 @@ contract FundsSplitter {
      * Procedure for withdrawing debited amount to the 
      * specific owner
      * @param amount Funds to withdraw
-     * @param sender Address of the transaction initiator
      */
-    function withdrawFunds(uint amount, address sender) whenNotPaused public{
-        // this function will withdraw whole funds associated with each
-        // address and transfer the amount to the resp
+    function withdrawFunds(uint amount) whenNotPaused public{
         require(amount > 0); // Funds can not be <= 0
-        bytes32 key = keccak256(sender,msg.sender);
-        require(splitTransactions[key].receiver == msg.sender,"[ER03] Invalid address");
-        require(splitTransactions[key].balance >= amount,"[ER03] Not enough Funds");
-        splitTransactions[key].balance -= amount;
-        splitTransactions[key].receiver.transfer(amount);        
+        require(balances[msg.sender] > 0, "[ER03] Invalid address");
+        require(balances[msg.sender] >= amount, "[ER04] Not enough Funds");
+        balances[msg.sender] -= amount;
+        msg.sender.transfer(amount);
     }
     
 
