c748ccd1808eee0d14f3cf3f5b63f3b2151e2a86
furqanbaqai <baqai.furqan@gmail.com>
2018-07-09 23:07:11 +0400

    BUG FIX! Fixing truffle migration js to comply with refactored approach

--- migrations/2_splitter_migration.js
+++ migrations/2_splitter_migration.js
@@ -2,7 +2,7 @@ var FundsSplitter = artifacts.require("./FundsSplitter.sol");
 
 module.exports = function(deployer, network, accounts) {
   console.log("Deploying Funds Splitter");
-  deployer.deploy(FundsSplitter, accounts[1], accounts[2],{
+  deployer.deploy(FundsSplitter,{
     gas: 1000000
   });
   /*deployer.deploy(FundsSplitter, {
