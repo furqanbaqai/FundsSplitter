d2c0b331535089dee78b3d3327a0916fd3c2c77a
furqanbaqai <baqai.furqan@gmail.com>
2018-07-10 18:36:51 +0400

    Revision#7: Incorporating latest comments

--- contracts/FundsSplitter.sol
+++ contracts/FundsSplitter.sol
@@ -63,9 +63,10 @@ contract FundsSplitter {
         require(receiver2 != address(0), "[ER02] Invalid address");
         require(receiver1 != receiver2, "[ER02] Invalid address");
         require(msg.value > 0, "[ER04] Invalid Values");
-        uint amount = msg.value / 2;                
-        balances[receiver1] += amount;
-        balances[receiver2] += msg.value - amount;
+        uint amount = msg.value.div(2);                
+        balances[receiver1] = balances[receiver1].add(amount);
+        balances[receiver2] = balances[receiver2].add(amount);
+        if(msg.value % 2 == 1) balances[msg.sender] = balances[msg.sender].add(1);
         emit LogSplit(msg.sender, receiver1,receiver2, msg.value);
         return true;
     }
@@ -75,13 +76,14 @@ contract FundsSplitter {
      * specific owner
      * @param amount Funds to withdraw
      */
-    function withdrawFunds(uint amount) whenNotPaused public{
+    function withdrawFunds(uint amount) whenNotPaused public returns(bool success){
         require(amount > 0); // Funds can not be <= 0
         require(balances[msg.sender] > 0, "[ER03] Invalid address");
         require(balances[msg.sender] >= amount, "[ER04] Not enough Funds");
         balances[msg.sender] -= amount;
         emit LogWithdrawl(msg.sender, amount);
         msg.sender.transfer(amount);
+        return true;
     }
     
 
